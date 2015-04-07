ec273a516fafeca00a53c84bbff6534f8ee0459b
AleksaT <aleksa.topalovic015@gmail.com>
2015-04-07 13:39:38 +0200

    Kreiran javadoc

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,26 +1,51 @@
 package com.twitter.poruke;
 
 public class TwitterPoruka {
+	/*
+	 * atribut korisnik
+	 */
 	private String korisnik;
+	/*
+	 * atribut poruka
+	 */
 	private String poruka;
+	/*
+	 * metoda getKorisnik
+	 * @return String vraca ime korisnika
+	 */
 	public String getKorisnik() {
 	return korisnik;
 	}
+	/*
+	 * metoda setKorisnik postavlja korisnik
+	 *@param korisnik unosi se ime korisnika
+	 *@throws RuntimeException ukoliko je korisnik nije unet ili je unet prazan String
+	 */
 	public void setKorisnik(String korisnik) {
 	if (korisnik==null || korisnik.equals(""))
 	throw new RuntimeException(
 	"Ime korisnika mora biti uneto");
 	this.korisnik = korisnik;
 	}
+	/*
+	 * metoda getPoruka 
+	 * @return String vraca sadrzaj poruke
+	 */
 	public String getPoruka() {
 	return poruka;
 	}
+	/*
+	 * metoda setPoruka
+	 * @param poruka unosi se sadrzaj poruke
+	 * @throws RuntimeException ukoliko poruka nije uneta ili ima vise od 140 znakova
+	 */
 	public void setPoruka(String poruka) {
 	if (poruka==null || poruka.length()>140)
 	throw new RuntimeException(
 	"Poruka mora biti uneta i mora imati najvise 140 znakova");
 	this.poruka = poruka;
 	}
+	
 	public String toString(){
 	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	} 
